487c6db5797f6ae4d3e9813a982b5a8604c0074f
Omkarlaxmansurvase <omkarsurvase2104@gmail.com>
2025-09-27 22:26:12 +0530

    lc 125

---
+++ src/com/dsa/lc125.java
@@ -0,0 +1,23 @@
+package com.dsa;
+
+import java.util.Locale;
+
+public class lc125 {
+    public static void main(String[] args) {
+
+    }
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase().replaceAll("[^A-Za-z0-9]","");
+        int i = 0;
+        int j=s.length()-1;
+
+        while(i<=j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
