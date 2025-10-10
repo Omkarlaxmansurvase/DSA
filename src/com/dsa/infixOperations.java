3ce2ad8e5db4c83e5a1e15083b9cbd5dd0db1daa
Omkarlaxmansurvase <omkarsurvase2104@gmail.com>
2025-10-11 00:48:52 +0530

    infix operations on stack with bracket priority

--- src/com/dsa/infixOperations.java
+++ src/com/dsa/infixOperations.java
@@ -3,7 +3,7 @@ package com.dsa;
 import java.util.*;
 public class infixOperations {
     public static void main(String[] args) {
-        String str ="9-5+3*4/6";
+        String str ="9-(5+3)*4/6";
         Stack<Integer> val =new Stack<>();
         Stack<Character> op =new Stack<>();
 
@@ -14,7 +14,19 @@ public class infixOperations {
             if(ascii>=48 && ascii<=57){
                 val.push(ascii-48); //this puts the actual no in stack
             }
-            else if  (op.size()==0 ) op.push(ch);
+            else if  (op.size()==0 || ch=='(' || op.peek()=='(' ) op.push(ch);
+            else if (ch==')'){
+                while(op.peek()!='('){
+                    int v2=val.pop();
+                    int v1=val.pop();
+                    if (op.peek()=='-') val.push(v1-v2);
+                    if (op.peek()=='+') val.push(v1+v2);
+                    if (op.peek()=='*') val.push(v1*v2);
+                    if (op.peek()=='/') val.push(v1/v2);
+                    op.pop();
+                }
+                op.pop(); //removed opening bracket here
+            }
             else{
                 if(ch=='+' || ch == '-'){
                     // do work
